737a5865128829611f4a4789b40813d59c6ef22e
pablo-fscs <pablo.fscs@gmail.com>
2022-08-06 19:22:33 -0300

    Manipulando microserviços com docker

---
+++ banco.sql
@@ -0,0 +1,10 @@
+CREATE TABLE dados (
+    ClienteID int,
+    Nome varchar(50),
+    Sobrenome varchar(50),
+    Endereco varchar(150),
+    Bairro varchar(100),    
+    Cidade varchar(50),
+    UF varchar(2),
+    Host varchar(50)
+);
